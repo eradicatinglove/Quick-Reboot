c900f3353fd45ba4953a5c0f5e05037bd4e68b70
eradicatinglove <58869530+eradicatinglove@users.noreply.github.com>
2023-07-25 15:24:42 -0500

    Add files via upload

--- source/main.cpp
+++ source/main.cpp
@@ -4,11 +4,15 @@
 #include <switch.h>
 
 //quick reboot switch
+//*for mariko reboot only*
 int main(int argc, char **argv)
 {
     consoleInit(NULL);
+   
     bpcInitialize();
+   
     bpcRebootSystem();
+   
     return 0;
 }
 
